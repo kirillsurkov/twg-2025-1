566e9a12633075cb90e9e48e0c72e62c74d078f0
kirillsurkov <surkovk20@yandex.ru>
2025-01-27 19:58:56 +0200

    Add PrimaryBlockPlugin

--- src/game.rs
+++ src/game.rs
@@ -1,11 +1,13 @@
-use bevy::prelude::*;
+use bevy::{prelude::*, utils::hashbrown::HashSet};
 use camera::GameCameraPlugin;
 use game_cursor::GameCursorPlugin;
 use light_consts::lux::AMBIENT_DAYLIGHT;
+use primary_block::{PrimaryBlock, PrimaryBlockPlugin};
 use room::RoomPlugin;
 
 mod camera;
 mod game_cursor;
+mod primary_block;
 mod room;
 
 pub struct GamePlugin;
@@ -13,11 +15,38 @@ pub struct GamePlugin;
 impl Plugin for GamePlugin {
     fn build(&self, app: &mut App) {
         app.add_plugins(GameCameraPlugin(Vec3::new(0.0, 0.0, 15.0)))
+            .add_plugins(PrimaryBlockPlugin)
             .add_plugins(RoomPlugin)
             .add_plugins(GameCursorPlugin)
             .add_systems(Startup, setup)
             .add_systems(Update, update_state)
+            .insert_resource(RoomLocations::default())
             .insert_resource(PlayerState::Idle);
+    }
+}
+
+#[derive(Resource, Default)]
+pub struct RoomLocations {
+    available: HashSet<IVec2>,
+    unavailable: HashSet<IVec2>,
+}
+
+impl RoomLocations {
+    fn validate_and_insert(&mut self, x: i32, y: i32) {
+        let value = IVec2::new(x, y);
+        if !self.unavailable.contains(&value) {
+            self.available.insert(value);
+        }
+    }
+
+    pub fn insert_around(&mut self, x: i32, y: i32) {
+        self.validate_and_insert(x, y + 1);
+        self.validate_and_insert(x, y - 1);
+        self.validate_and_insert(x + 1, y);
+        self.validate_and_insert(x - 1, y);
+        let value = IVec2::new(x, y);
+        self.available.remove(&value);
+        self.unavailable.insert(value);
     }
 }
 
@@ -38,6 +67,7 @@ fn setup(mut commands: Commands) {
         },
         Transform::from_xyz(-1.0, -1.0, 10.0).looking_at(Vec3::ZERO, Vec3::Z),
     ));
+    commands.spawn(PrimaryBlock { x: 0, y: 0 });
 }
 
 #[derive(Resource)]
